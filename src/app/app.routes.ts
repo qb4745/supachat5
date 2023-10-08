d50f02262e74fc53eebb55ece13150996fc02b67
Jaime Vicencio <jai.vicencio@duocuc.cl>
2023-10-08 13:11:49 -0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,20 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'groups',
+    loadComponent: () => import('./pages/groups/groups.page').then( m => m.GroupsPage)
+  },
+  {
+    path: 'messages',
+    loadComponent: () => import('./pages/messages/messages.page').then( m => m.MessagesPage)
+  },
 ];
---
+++ src/app/pages/groups/groups.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-groups',
+  templateUrl: './groups.page.html',
+  styleUrls: ['./groups.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class GroupsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
